946671c2ef5b9a4c8ab7dbcfe9f3595b12226f3f
Muhammad Khizar <79244644+khizr45@users.noreply.github.com>
2024-09-14 22:13:52 +0500

    docker container

--- cv_to_json.py
+++ cv_to_json.py
@@ -45,4 +45,4 @@ def cv_to_json():
     return jsonify({'message': 'Invalid file type, please upload a PDF'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8000,debug=True)
